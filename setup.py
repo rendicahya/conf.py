8b8d8449d0926d3382be59435a747a92eff8d431
Randy Cahya Wihandika <rendicahya@gmail.com>
2023-03-15 15:51:34 +0900

    Renaming from conf.py to config.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="conf.py",
+    name="config.py",
     version="0.1.0",
     author="Randy Cahya Wihandika",
     author_email="rendicahya@gmail.com",
